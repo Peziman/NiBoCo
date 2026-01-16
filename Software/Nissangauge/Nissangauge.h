ecdd4d7c10fc3e48d98bdd5a991d1fc7b418840d
Peziman <44964725+Peziman@users.noreply.github.com>
2026-01-16 14:15:50 +0100

    Update Nissangauge.h

--- Software/Nissangauge/Nissangauge.h
+++ Software/Nissangauge/Nissangauge.h
@@ -1,5 +1,5 @@
 #define SERIAL_UPDATE_MS  100
-#define N_PAGES 5
+#define N_PAGES 6
 #define Btn_next 2 //Button 1 (Next)
 #define Btn_prev 4 //Button 2 (Prev)
 #define Btn_push 12 //Button 3 (Enter)
@@ -7,6 +7,8 @@
 
 #define REF_VOLTAGE 5.0 //Reference voltage for car battery voltage calculations
 #define PIN_STEPS 1024.0 ////Reference steps for car battery voltage calculations
+
+#define BUTTON_HOLD_TIME 3000
 
 /* ----- Engine status ----- */ //FOR LATER USE
 #define ENGINE_OFF		0 
@@ -17,3 +19,5 @@
 #define IDLE_RPM_EXIT   1100
 #define IDLE_DELAY_MS   2000
 /* ----- Engine status end ----- */
+
+uint16_t get_VOLT();
