e586c46865b503701e704dfa0623ae1f06f8d8fe
Peziman <44964725+Peziman@users.noreply.github.com>
2026-01-16 14:18:52 +0100

    Update Pump_control.ino

--- Software/Nissangauge/Pump_control.ino
+++ Software/Nissangauge/Pump_control.ino
@@ -7,8 +7,10 @@ bool after_run = false;
 bool warm_dis = false;
 uint8_t get_CLT = 0;
 uint8_t pump_duty = 0;
+uint8_t aux_duty = 0;
 bool fan_on = false;
 int16_t PID;
+//uint16_t auxspeed_2 = 0;
 
 void initPump() 
 {
@@ -21,7 +23,7 @@ void initPump()
   pinMode(In_run, INPUT);
   
   //pump start up see CWA200 pump docs
-  //analogWrite(Out_pump_pwm, 0); 
+  // simulate LIN-Bus commands to wakeup and setup PWM mode 
   analogWrite(Out_pump_pwm, 0); //100% PWM Pumpspeed
   digitalWrite(Out_led_r, HIGH); //LED indicator
   delay(300); //for 300ms
@@ -44,7 +46,7 @@ uint16_t get_pot(uint8_t a_input, uint8_t pwm1, uint8_t pwm2) // get values from
   return sensorValue;
 }
 
-uint16_t get_pot_duty(uint8_t a_input) //for display
+uint16_t get_pot_duty(uint8_t a_input) //for display return the current position of potentiometer
 {
   uint16_t high_pot_duty;
   uint16_t low_pot_duty;
@@ -61,10 +63,32 @@ uint16_t get_pot_duty(uint8_t a_input) //for display
   }
 }
 
+uint16_t test_pot_duty(uint8_t a_input) //for display return the current position of potentiometer in testmode
+{
+  uint16_t high_pot_duty_t;
+  uint16_t low_pot_duty_t;
+  low_pot_duty_t = map(analogRead(In_low_ref), 0, 1023, 0, 100);
+  high_pot_duty_t = map(analogRead(In_high_ref), 0, 1023, 0, 100);
+
+  if (a_input == false)
+  {
+    return low_pot_duty_t;
+  }
+  else
+  {
+    return high_pot_duty_t;
+  }
+}
+
 
 uint8_t get_pump_duty() //for display return the current  duty of the pump
 {
   return pump_duty;    
+}
+
+uint8_t get_aux_duty() //for display return the current  duty of the auxpump
+{
+  return aux_duty;    
 }
 
 bool get_fan_state() //for display get the actually state of the cooling fan
@@ -101,6 +125,18 @@ bool get_warmup() // for display return the state of the warmup mode
   return warm_dis;
 }
 
+bool get_overtemp()
+{
+  if (get_CLT > target_temp + over_temp)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 bool get_Temp_sens_state(uint8_t Temp) //for display return if a temp sensor is damaged or not connected 
 {
   if (Temp == 255)
@@ -112,6 +148,8 @@ bool get_Temp_sens_state(uint8_t Temp) //for display return if a temp sensor is 
     return false;
   }
 }
+
+
 
 void runPump()
 {
@@ -130,7 +168,6 @@ void runPump()
    
   uint16_t get_CLT = GETDATA(0); 
 
-  
  
   if (get_CLT <= target_temp - heatup_temp & get_CLT <= target_temp - heatup_temp2 && overheat == false)
   {      
@@ -146,6 +183,7 @@ void runPump()
     analogWrite(Out_led_g, 0); 
     digitalWrite(Out_led_r, LOW);
     digitalWrite(Out_afterrun_on, LOW);
+    set_auxspeed(aux_low, 3);
     
     if (pumpTime == 0) 
     {
@@ -198,6 +236,7 @@ void runPump()
     PID = get_PWM(0,0,0,false);
     get_coldspeed = get_pot(In_low_ref, lowspeed, hot_speed);
     set_pumpspeed(get_coldspeed);
+    set_auxspeed(aux_norm, 3);
   }  
 
   else if (get_CLT >= target_temp - heatup_temp2 & get_CLT < target_temp + over_temp)  
@@ -219,6 +258,8 @@ void runPump()
     //p_value = map(get_CLT, 63, 90, 50, 200);
     p_value = get_PWM(get_CLT, get_coldspeed, get_warmspeed, true);
     //p_value = constrain(map(get_CLT, target_temp - heatup_temp2, target_temp, get_coldspeed, get_warmspeed), lowspeed, get_warmspeed);
+    
+    // for future add speed at some conditions of driving
     if (add_rpm > rpm_limit)
     {
       p_value = p_value + add_rpm_v;
@@ -227,7 +268,9 @@ void runPump()
     {
       p_value = p_value + add_tps_v;
     }
+
     set_pumpspeed(p_value);
+    set_auxspeed(aux_norm, 3);
     led_value = map(p_value, no_speed, get_warmspeed, led_g_min, led_g_max);
     analogWrite(Out_led_g, led_value);
       
@@ -239,6 +282,7 @@ void runPump()
   // status RGB Led shows red   
   {
     set_pumpspeed(hot_speed);
+    set_auxspeed(aux_hot, 3);
     overheat = true;
     after_run = true;
     warm_up = false;
@@ -291,6 +335,7 @@ void afterrun_condition (bool (condition)(void))
 {
   uint16_t cnt1 = 0;
   uint16_t cnt2 = 0;
+  uint8_t cnt3 = 0;
   uint8_t led_b = 0;
   uint8_t led_g = 30;
 
@@ -306,17 +351,27 @@ void afterrun_condition (bool (condition)(void))
     // till engine is hot pump runs at high speed an coolant fan is running 
     {
       digitalWrite(Out_fan, HIGH);
-      set_pumpspeed(hot_speed);                
+      set_pumpspeed(hot_speed);
+      set_auxspeed(aux_hot, 3);
+      cnt3 = 0;                
     } 
     else
     // engine isn't hot. pump runs at a slower speed
     {
-      digitalWrite(Out_fan, LOW);
+      if (digitalRead(Out_fan) && cnt3 > 30)
+      {
+        digitalWrite(Out_fan, LOW);
+        cnt3 = 0;
+      }
+      cnt3 = cnt3 + 1;
       set_pumpspeed(midspeed);
+      set_auxspeed(aux_norm, 3);
     } 
     if (cnt1 > afterrun_time)
     {
+      // shutdown everthing
       analogWrite(Out_pump_pwm, 0);
+      analogWrite(Out_aux_pwm, 0);
       digitalWrite(Out_fan, LOW);
       analogWrite(Out_led_b, 0);
       digitalWrite(Out_led_r, LOW);
@@ -340,13 +395,18 @@ void afterrun_condition (bool (condition)(void))
       }        
     }
 
-    if (digitalRead(12) == true) 
+    if (digitalRead(12) == true || get_CLT < target_temp - heatup_temp2) // button pressed to or temp low... abort
     {
       cnt1 = afterrun_time;
     }
- 
-
+  
     Afterrun_DISP(F("Time Till Shutdown"),afterrun_time - cnt1, 0, afterrun_time);
+    Serial.print("Time: ");
+    Serial.println(cnt3);
+    Serial.print("CLT: ");
+    Serial.print(get_CLT);
+    Serial.println(" C°");
+    
     delay(100);    
   }
 }
@@ -359,12 +419,46 @@ void afterrun_control () // afterrun control function
 void set_pumpspeed (uint8_t pumpspeed) 
 // set the pump speed to PWM
 { 
-  pumpspeed = constrain(pumpspeed, no_speed, max_speed);  // Begrenzt den Bereich der PWM Ausgabe auf 34 bis 220.
+  pumpspeed = constrain(pumpspeed, no_speed, max_speed);  // limit max PWM output to 34 bis 220.
   analogWrite(Out_pump_pwm, 255 - pumpspeed); //PWM value negated
   pump_duty = map(pumpspeed, no_speed, max_speed, 0, 100);  // Value for display
-  Serial.print("Pumpspeed: ");
-  Serial.print(pump_duty);
-  Serial.println("%");
+  //Serial.print("Pumpspeed: ");
+  //Serial.print(pump_duty);
+  //Serial.println("%");
+}
+
+void set_auxspeed (uint8_t auxspeed, uint8_t time_factor) 
+// set the AUX pump speed to PWM
+{ 
+  static int16_t auxspeed_2 = 0;
+  //Serial.print("SOLL Speed: ");
+  //Serial.println(auxspeed);
+  //Serial.print("IST Speed:");
+  //Serial.println(auxspeed_2);
+
+  if (auxspeed > auxspeed_2) //soft start
+    {
+      auxspeed_2 = auxspeed_2 + time_factor;
+      if (auxspeed_2 > 255)
+      {
+        auxspeed_2 = 255;
+      }
+    }
+  else if (auxspeed < auxspeed_2)
+    {
+      auxspeed_2 = auxspeed_2 - time_factor;
+      if (auxspeed_2 < 0)
+      {
+        auxspeed_2 = 0;
+      }
+    }
+   
+  
+  auxspeed_2 = constrain(auxspeed_2, 0, 255);  // limit max PWM output
+  analogWrite(Out_aux_pwm, auxspeed_2); //PWM value
+  aux_duty = map(auxspeed_2, 0, 255, 0, 100);  // Value for display 
+  //Serial.println(auxspeed_2);
+  //Serial.println(auxspeed);
 }
   
 uint16_t GETDATA (uint8_t x)
@@ -410,9 +504,11 @@ int16_t get_PWM (uint16_t clt, uint16_t low_point, uint16_t high_point, bool run
   //static int16_t PID;
   uint16_t PID2;
   static int8_t t_error_old = 0;
+  static int8_t t_error_old2 = 0;
   static int16_t I = 0;
   static uint8_t i_cnt = 0;
   static uint8_t d_cnt = 0;
+
 
   if (run == false)
   {
@@ -436,45 +532,71 @@ int16_t get_PWM (uint16_t clt, uint16_t low_point, uint16_t high_point, bool run
     //I part calculation
     if (i_cnt > i_loops)
     {
-      I += t_error * ki;
-      i_cnt = 0;
+      if (PID > -1000 && PID < 0)
+      {
+        I += t_error * ki;
+        i_cnt = 0;
+      }
+      else
+      {
+        I = constrain(I, -1000, 0);
+      }
+
     }
 
     //D part calculation
     if (d_cnt > d_loops)
     {
-      if (t_error - t_error_old < 0)
+      if (t_error - t_error_old2 < 0)
       {
-        D = (t_error - t_error_old) * kd;
+        D = (t_error - t_error_old2) * kd;
 
       }
       else
       {
         D = 0;
       }
-      t_error_old = t_error;
+      t_error_old2 = t_error;
       d_cnt = 0;
       
     }
+
+    t_error_old = t_error;
 
     //PID calulation
     PID = P + I + D;  // + I + D;
     PID = constrain(PID, -1000, 0);
     PID2 = map(PID, -1000, 0, low_point, high_point);
-    Serial.print("P: ");
-    Serial.println(P);
-    Serial.print("I: ");
-    Serial.println(I);
-    Serial.print("D: ");
-    Serial.println(D);
-    Serial.print("PID2: "); 
-    Serial.println(PID);
-  }
 
-  
+
+  }  
   return PID2;
+}
 
+void testmode_init()
+{
+  //shutdown everthing
+  analogWrite(Out_pump_pwm, 0);
+  analogWrite(Out_aux_pwm, 0);
+  analogWrite(Out_led_b, 0);
+  digitalWrite(Out_led_r, LOW);
+  analogWrite(Out_led_g, 0);
 
+  //turn on Fan
+  digitalWrite(Out_fan, HIGH);
 
+  //turn off afterrun function
+  digitalWrite(Out_afterrun_on, LOW); 
+}
 
+void testmode() //Pump tests
+{
+  uint16_t get_pumpspeed = get_pot(In_high_ref, 0, 255);
+  uint16_t get_time = get_pot(In_low_ref, 0, 10);
+  set_auxspeed(constrain(get_pumpspeed, aux_min, aux_hot), get_time);
+  set_pumpspeed(map(get_pumpspeed, 0, 255, no_speed, max_speed));
+  digitalWrite(Out_led_r, HIGH);
+  digitalWrite(Out_led_b, HIGH);
+  Serial.print("Time: ");
+  Serial.println(get_time);
 }
